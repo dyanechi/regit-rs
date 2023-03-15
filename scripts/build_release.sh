70588488d6bce1596b7d232a9e9ba5cab6c9c955
dyanechi <dyanechi@proton.me>
2023-03-15 16:39:44 +0000

    update script

--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PROJECT_DIR="$SCRIPT_DIR/.."
 
 APP_NAME="regit"
 APP_VER="v0.1.0"
 APP_DESC="A tool for fetching git directories"
-APP_FILE="./target/release/$APP_NAME"
+APP_FILE="$PROJECT_DIR/target/release/$APP_NAME"
 
 RELEASE_NAME="$APP_NAME-$APP_VER"
-RELEASE_DIR="./release/$RELEASE_NAME"
+RELEASE_DIR="$PROJECT_DIR/release/$RELEASE_NAME"
 
 sudo echo ""
 echo "Building cargo package in release mode..."
@@ -21,5 +22,11 @@ fi
 
 cp "$SCRIPT_DIR/install_release.sh" "$RELEASE_DIR/install.sh"
 cp $APP_FILE "$RELEASE_DIR/$APP_NAME"
+
+cd "release/"
+tar czf "$RELEASE_NAME.tar.gz" $RELEASE_NAME
+# mv "$RELEASE_NAME.tar.gz" $RELEASE_DIR
+rm -rf $RELEASE_NAME
+cd ..
 
 echo "Done!"
